83cf2e48a719ae9f5bcaef241f9e1586c33084a8
LarsBracke <lars.bracke@student.howest.be>
2022-06-01 17:44:00 +0200

    UI Updates.

--- Game/OverlordProject/Prefabs/InGameHudPrefab.cpp
+++ Game/OverlordProject/Prefabs/InGameHudPrefab.cpp
@@ -2,16 +2,30 @@
 #include "InGameHudPrefab.h"
 
 #include "Helpers/GameManager.h"
+#include "Components/SpriteComponent.h"
 
 void InGameHudPrefab::Initialize(const SceneContext&)
 {
-	auto* pHUD = new GameObject();
-	AddChild(pHUD);
-	pHUD->AddComponent(new SpriteComponent(L"Textures/UI_Background.png"));
-	pHUD->GetTransform()->Translate(0, 720 - 193, 0);
+	//auto* pHUD = new GameObject();
+	//AddChild(pHUD);
+	//pHUD->AddComponent(new SpriteComponent(L"Textures/UI_Background.png"));
+	//pHUD->GetTransform()->Translate(0, 720 - 193, 0);
 }
 
 void InGameHudPrefab::PostInitialize(const SceneContext&)
 {
 	m_pKirbyHealthComponent = GameManager::Get()->GetKirby()->GetComponent<HealthComponent>();
+	m_HealthAmount = m_pKirbyHealthComponent->GetHealth();
+
+	AddHealthElement(XMFLOAT3{ 10, 720 - 75, 0 });
+	AddHealthElement(XMFLOAT3{ 75, 720 - 75, 0 });
+}
+
+GameObject* InGameHudPrefab::AddHealthElement(const XMFLOAT3& position)
+{
+	auto* pElement = AddChild(new GameObject());
+	pElement->AddComponent(new SpriteComponent(L"Textures/HealthElement.png"));
+	pElement->GetTransform()->Translate(position);
+
+	return pElement;
 }
--- Game/OverlordProject/Prefabs/InGameHudPrefab.h
+++ Game/OverlordProject/Prefabs/InGameHudPrefab.h
@@ -14,5 +14,11 @@ protected:
 
 private:
 	HealthComponent* m_pKirbyHealthComponent{ nullptr };
+
+	UINT m_HealthAmount{ };
+	std::vector<GameObject*> m_pHealthElements{ };
+	const UINT m_ElementOffset{ 125 };
+
+	GameObject* AddHealthElement(const XMFLOAT3& position);
 };
 
